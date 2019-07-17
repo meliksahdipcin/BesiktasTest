ea7467b03da5da59021a87d9dd053a317e37ea31
Melikşah Dipçin <meliksahdipcin@gmail.com>
2019-07-17 10:13:44 +0300

    Initial Commit

---
+++ deneme2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deneme2
+//
+//  Created by Melikşah on 17.07.2019.
+//  Copyright © 2019 Dipcin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
